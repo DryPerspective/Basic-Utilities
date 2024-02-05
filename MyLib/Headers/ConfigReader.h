b7432fd74dbf929ec06446f1942a66d8ec5cace4
DryPerspective <41272259+DryPerspective@users.noreply.github.com>
2024-02-05 17:02:01 +0000

    Complete rewrite of ConfigReader.
    New version is less clunky and awkward.

--- MyLib/Headers/ConfigReader.h
+++ MyLib/Headers/ConfigReader.h
@@ -7,279 +7,155 @@
  In short, many of my projects need to read a file of the format:
  Variable1=[Value]
  Variable2=[Value]
- A few notes on file layout - by default whitespace and empty lines are ignored by the program, and lines in the config file can be "commented" by starting them with #
+ A few notes on file layout - whitespace and empty lines are ignored by the program, and lines in the config file can be "commented" by starting them with #
  In order to configure themselves and get the correct values into the program. This class is intended to take a file of that layout and automate the reading of it.
 
- This class does appear from the outset to be rather memory-heavy. And to an extent this is true - it internally stores a map of strings of all values in the input
- file at once. However the intention is that the entire ConfigReader class can be created, its values read, and then it (implicitly) destructed in a single try block
- near the beginning of the program. It also includes a close() member function to empty the map if direct control over that is needed.
+ Values can be retrieved via the readValue member function. By default this will retrieve a string corresponding to the key in the file, however templated versions
+ are included which perform conversion to an appropriate type
 
- The main readValue function supports the following types:
- * Fundamental integer and floating point types, written in base-10, scientific, or hex notation, though hex must be prefixed with "0x" in the file
- * Char
- * Bool
- * std::string
- In addition it also supports genertic types T which meet *any* of the following criteria
- * Must be assignable from std::string, i.e. std::is_assignable_v<T, std::string> must be true.
- * Must be constructable from a std::string_view and no other parameters, and must be either move- or copy-assignable, with move-assignment taking priority.
- * Must be constructable from a std::string and no other parameters, and must be either move- or copy-assignable, with move-assignment taking priority.
- * Must be extractable from an std::istream using operator>>, such that an extraction with types std::string >> T will evaluate correctly.
 */
 
-
-#include<fstream>
-#include<string>
-#include<string_view>
-#include<unordered_map>
-#include<exception>
-#include<charconv>
-#include<regex>
-#include<type_traits>
-#include<stdexcept>
-#include<sstream>
-#include<filesystem>
+#include <type_traits>
+#include <filesystem>
+#include <unordered_map>
+#include <string>
+#include <optional>
+#include <charconv>
 
 #include "Traits.h"
 
+namespace dp {
 
-
-	namespace dp {
-
-		class ConfigReader
-		{
-
-		public:
-			//Configuration flags. Declared here so we can use them as private member data.
-			enum flags : unsigned char {
-				noFlagsActive = 0,
-				removeAllWs = 0b00000001,		//Remove white space from inside the text in the config file. e.g. Message = Hello World -> {Message,HelloWorld}
-				keepAllPadding = 0b00000010,		//Keep any white space "padding" left between the equals and the value and after the value in the config file, e.g. Message = Hello World -> {Message , Hello World}
-				caseInsensitive = 0b00000100,		//Set all text read in to lower case for both storage and retrieval. This means input is not case sensitive.
-			};
-
-
-		private:
-
-
-
-			//Encapsulation as we want a complete wrapper around the data container and want to prevent external meddling with it.
-			std::unordered_map<std::string, std::string>	m_values;
-			ConfigReader::flags								m_flags;
-
-			//Adds all data from a file to the m_values map. Separated from constructor for use with addFile function.
-			void addFileToMap(std::string_view fileName);
-
-
-			//Simple function to trim any leading or trailing whitespace from a string,
-			static std::string trim(const std::string& str, std::string_view whitespace = " \f\n\r\t\v");
-
-			//Sets all text to lower case, used for the caseInsensitive flag
-			static void toLower(std::string& inString);
-
-			//This function is called to read a string representing a number into a numerical type.
-			template<typename T>
-			constexpr T readNumerical(std::string_view valueInFile) const {
-
-				//Then use from_chars to read it.
-				T output{};
-				std::from_chars_result result;
-
-
-				//Integer and floating point values use from_chars differently, so we separate here
-				if constexpr (std::is_floating_point<T>::value) {
-
-					//First deduce the format of our final result.
-					auto format{ std::chars_format::general };
-					//Regex for scientific format numbers. It can contain any number of numerical digits, optionally a decimal point, either e or E for exponent, optionally a -, then any number of digits.
-					std::regex scientific{ R"(\-?\d+\.?\d*[eE]\-?\d+)" };
-					//Ditto for hex. Assumes that hex numbers are prefixed with "0x" in the file
-					std::regex hex{ R"(0x\-?[\da-fA-F]+\.?[\da-fA-F]*)" };
-					if (std::regex_match(std::string(valueInFile), scientific))format = std::chars_format::scientific;
-					else if (std::regex_match(std::string(valueInFile), hex)) {
-						format = std::chars_format::hex;
-						//We need to remove the 0x prefix as from_chars can't parse it.
-						valueInFile.remove_prefix(2);
-					}
-					//std::string readString{ valueInFile };
-					result = std::from_chars(valueInFile.data(), valueInFile.data() + valueInFile.length(), output, format);
-				}
-				else {
-					result = std::from_chars(valueInFile.data(), valueInFile.data() + valueInFile.length(), output);
-				}
-
-				//Now onto error handling and ensuring that we got the right result.
-				//As the success largely depends on the user entering the correct data in the input file, we throw exceptions when they do not.
-				if (result.ptr == valueInFile.data() + valueInFile.length()) return output;
-				else if (result.ec == std::errc::invalid_argument) {
-					std::string errMsg{ "Error: Value " };
-					errMsg += valueInFile;
-					errMsg += " in file is of an invalid format.";
-					throw ConfigReader::ConfigException(errMsg);
-				}
-				else if (result.ec == std::errc::result_out_of_range) {
-					std::string errMsg{ "Error: Value " };
-					errMsg += valueInFile;
-					errMsg += " is larger than the type it is being read as.";
-					throw ConfigReader::ConfigException(errMsg);
-
-				}
-				//This will never trigger but we need to return something from all paths.
-				return output;
-			}
-
-
-
-		public:
-
-			//We don't want default or copy-instantiation so we disable those constructors.
-			ConfigReader() = delete;
-			ConfigReader(const ConfigReader&) = delete;
-
-			//There's no particular reason that move construction should be forbidden, though its expected use will be a bit niche.
-			ConfigReader(ConfigReader&&) noexcept = default;
-
-			//We only want construction when we have a specific file to open and read.
-			//ConfigReader(std::string_view fileName, ConfigReader::flags inFlags = flags::noFlagsActive);
-			ConfigReader(std::filesystem::path file, ConfigReader::flags inFlags = flags::noFlagsActive);
-
-			 ~ConfigReader() noexcept = default;
-
-			//Free up memory from the map for cases where we are done with the ConfigReader but it remains in scope
-			void close();
-
-			//Read an additional file and insert its data into the map.
-			//void addFile(std::string_view fileName);
-			void addFile(std::filesystem::path fileName);
-
-			std::string_view getValue(std::string_view key);
-
-
-			//This is the core function. It it the one-size-fits-all function to match a string which was in the file to some variable within the program.
-			//It has support for numeric types, strings, and PhysicsVector objects. As we're ostensibly reading from strings, exotic custom types can be read
-			//by returning the string and processing it in the main project file.
-			template <typename T>
-			void readValue(std::string varNameInFile, T& inVariable) const {
-				if (m_flags & flags::caseInsensitive) {
-					toLower(varNameInFile);
-				}
-				std::string valueInMap;
-				try {
-					valueInMap = m_values.at(varNameInFile);
-				}
-				catch (const std::out_of_range&) {
-					std::string errMsg{ "Error: Attempting to match value " };
-					errMsg += varNameInFile;
-					errMsg += " however this does not appear in the config file.";
-					throw ConfigReader::ConfigException(errMsg);
-				}
-
-
-				//Different types handle differently. Char comes first as it would also be caught in is_arithmetic
-				if constexpr (std::is_same<T, char>::value) {
-					inVariable = valueInMap[0];
-				}
-				//std::string
-				else if constexpr (std::is_same_v<T, std::string>) {
-					inVariable = valueInMap;
-				}
-				//Bool.
-				else if constexpr (std::is_same_v<T, bool>) {
-					switch (valueInMap[0]) {
-					case 't':	//true
-					case 'T':
-					case 'y':	//yes
-					case 'Y':
-					case '1':	//generic 1
-						inVariable = true;
-						break;
-					case 'f':	//false
-					case 'F':
-					case 'n':	//no
-					case 'N':
-					case '0':	//generic 0
-						inVariable = false;
-						break;
-					default:
-						throw ConfigReader::ConfigException("Error: Attempting to read bool but variable in file is set to incorrect value.");
-					}
-				}
-				//Numerical types
-				else if constexpr (std::is_arithmetic_v<T>) {
-					inVariable = readNumerical<T>(valueInMap);
-				}
-				//GENERIC TYPES
-				//Assigned directly by a std::string
-				else if constexpr (std::is_assignable_v<T, std::string>) {
-					inVariable = valueInMap;
-				}
-				//Constructble from a string_view along and can be move assigned
-				else if constexpr (std::is_constructible_v<T, std::string_view> && std::is_move_assignable_v<T>) {
-					inVariable = std::move(T{ valueInMap });
-				}
-				else if constexpr (std::is_constructible_v<T, std::string_view> && std::is_copy_assignable_v<T>) {
-					//Note we need to make a temporary as we need an lvalue, since this branch can trigger if the class
-					//explicitly cannot move-assign.
-					T temp{ valueInMap };
-					inVariable = temp;
-				}
-				//Constructible from a string alone and can be move assigned
-				else if constexpr (std::is_constructible_v<T, std::string> && std::is_move_assignable_v<T>) {
-					inVariable = std::move(T{ valueInMap });
-				}
-				//Constructable from a string alone and can be copy assigned.
-				else if constexpr (std::is_constructible_v<T, std::string> && std::is_copy_assignable_v<T>) {
-					//Again we need an lvalue here.
-					T temp{ valueInMap };
-					inVariable = temp;
-				}
-				else if constexpr (dp::isExtractible_v<T>) {
-					std::stringstream sstr;
-					sstr << valueInMap;
-					sstr >> inVariable;
-				}
-				/*
-				//C-srings
-				else if constexpr (std::is_array_v<T> && std::is_same_v<char&, decltype(*inVariable)>) {
-					if (valueInMap.length() > strlen(inVariable)) throw ConfigReader::ConfigException("Error: Attempting to match variable in file to a C-string which is too short to contain it");
-					else strcpy_s(inVariable, valueInMap.c_str());
-				}
-				*/
-				else static_assert(dp::dependentFalse<T>, "ConfigReader::readValue called with unsupported type");
-
-			}
-
-			//A more conventional method without an in-out parameter. Logically this can only apply to default constructble and copyable types, so we filter for those
-			//(and hope that RVO saves us a copy anyway)
-			template<typename T, std::enable_if_t<std::is_default_constructible_v<T> && std::is_copy_constructible_v<T>,bool> = true>
-			T readValue(std::string_view VarNameInFile) {
-				T result{};
-				readValue(VarNameInFile, result);
-				return result;
-			}
-
-
-
-			//A custom exception to handle errors specific to ConfigReader operation.
-			class ConfigException : public std::exception {
-			private:
-				std::string m_message;
-
-			public:
-				ConfigException(std::string_view message) : m_message{ message } {};
-
-				virtual ~ConfigException() = default;
-
-				const char* what() const noexcept override;
-			};
-
-
-
-		};
-
+	//String_view starts_with functionality, here until I find a better place to put it
+	inline constexpr auto StartsWith(std::string_view src, std::string_view prefix) -> bool {
+		return std::string_view{ src.data(), std::min(src.size(), prefix.size()) } == prefix;
 	}
 
 
+	class ConfigReader {
+
+		using map_type = std::unordered_map<std::string, std::string>;
+		map_type file_contents;
+
+		//Value lookup from a key
+		auto GetValue(std::string_view key) const -> std::optional<std::string_view> ;
+
+		template<typename Dest>
+		static constexpr auto FromString(std::string_view in) -> Dest {
+			//Easy conversion first
+			if constexpr (std::is_convertible_v <std::string_view, Dest>) {
+				return static_cast<Dest>(in);
+			}
+			//Integral types via charconv
+			else if constexpr (std::is_integral_v<Dest>) {
+				auto base{ 10 };
+				if (StartsWith(in, "0x")) {
+					base = 16;
+					in.remove_prefix(2);
+				}
+				Dest ret{};
+				auto result{ std::from_chars(in.data(), in.data() + in.size(), ret, base) };
+				if (result.ec == std::errc{}) {
+					return ret;
+				}
+				else if(result.ec == std::errc::invalid_argument){
+					throw ConfigException{ "Could not convert " + std::string{in} + ": Invalid argument" };
+				}
+				else {
+					throw ConfigException{ "Coult not convert " + std::string{in} + ": Out of range" };
+				}
+			}
+			//Floating point types via charconv
+			else if constexpr (std::is_floating_point_v<Dest>) {
+				std::chars_format fmt{ std::chars_format::general };
+				if (StartsWith(in, "0x")) {
+					fmt = std::chars_format::hex;
+					in.remove_prefix(2);
+				}
+				Dest ret{};
+				auto result{ std::from_chars(in.data(), in.data() + in.size(), ret, fmt)};
+				if (result.ec == std::errc{}) {
+					return ret;
+				}
+				else if (result.ec == std::errc::invalid_argument) {
+					throw ConfigException{ "Could not convert " + std::string{in} + ": Invalid argument" };
+				}
+				else {
+					throw ConfigException{ "Coult not convert " + std::string{in} + ": Out of range" };
+				}
+			}
+			else {
+				//I hate this ugly hack.
+				static_assert(dependentFalse<Dest>, "FromString called with invalid type");
+			}
+		}
 
 
+	public:
+
+		ConfigReader(std::filesystem::path in_path);
+
+		auto addFile(std::filesystem::path in_path) -> void;
+
+		auto clear() -> void;
+
+		template<typename T>
+		constexpr auto readValue(std::string_view in) const -> T {
+			auto val = GetValue(in);
+			if (!val.has_value()) throw ConfigException("Value " + std::string{ in } + " not found in map");
+			return FromString<T>(*val);
+		}
+
+		template<typename T>
+		constexpr auto readValue(std::string_view in, const T& default_value) const noexcept -> T {
+			auto val = GetValue(in);
+			if (!val.has_value()) return default_value;
+			try {
+				return FromString<T>(*val);
+			}
+			catch (...) {
+				return default_value;
+			}
+		}
+
+		//Non-template approach just fetches the key
+		constexpr auto readValue(std::string_view in) const -> std::string_view {
+			return readValue<std::string_view>(in);
+		}
+
+		struct ConfigException : public std::runtime_error {
+			using Base = std::runtime_error;
+
+			using Base::Base;
+		};
+
+
+
+		//A few deprecated calls for backwards compatibility
+		//To be removed in a future update
+		enum flags : unsigned char { //This was always an overdesign
+			noFlagsActive = 0,
+			removeAllWs = 0b00000001,		
+			keepAllPadding = 0b00000010,		
+			caseInsensitive = 0b00000100,		
+		};
+
+		[[deprecated("ConfigReader flags have been removed")]]
+		ConfigReader(std::filesystem::path in_path, flags) : ConfigReader(in_path) {}
+
+		[[deprecated("This function is deprecated. Use clear() instead")]]
+		auto close() -> void;
+
+		[[deprecated("This function is deprecated. Use readValue instead")]]
+		constexpr auto getValue(std::string_view in) -> std::string_view {
+			return readValue(in);
+		}
+
+
+	};
+
+
+
+}
 
 #endif
--- MyLib/Source Files/ConfigReader.cpp
+++ MyLib/Source Files/ConfigReader.cpp
@@ -1,101 +1,185 @@
 #include "ConfigReader.h"
 
 #include <cctype>
+#include <cwctype>
+#include <fstream>
+#include <string_view>
+#include <algorithm>
+
+//These are internal to the workings of the class. The interface need not know about them.
+namespace {
+
+	//To prevent duplication
+	constexpr auto first_non_space(std::string_view str) {
+		//No constexpr find_if until C++20
+#if __cplusplus >= 202002L
+		//Grab the first non-ws character (don't forget cctype functions are a bit cursed
+		return std::find_if(str.begin(), str.end(), [](char in) -> bool {return !std::isspace(static_cast<unsigned char>(in)); });
+#else
+		if (str.empty()) return str.end();
+		for (std::string_view::const_iterator it = str.begin(); it != str.end(); ++it) {
+			if(! static_cast<bool>(std::isspace(static_cast<unsigned char>(*it)))) return it;
+		}
+
+		return str.end();
+
+#endif
+	}
+
+	constexpr auto last_non_space(std::string_view str) {
+#if __cplusplus >= 202002L
+		return std::find_if(str.rbegin(), str.rend(), [](char in) -> bool {return !std::isspace(static_cast<unsigned char>(in)); });
+#else
+		if (str.empty()) return str.rend();
+		for (std::string_view::const_reverse_iterator it = str.rbegin(); it != str.rend(); ++it) {
+			if (!static_cast<bool>(std::isspace(static_cast<unsigned char>(*it)))) return it;
+		}
+
+		return str.rend();
+
+#endif
+	}
+
+	constexpr auto trim(std::string_view in) -> std::string_view {
+		if (in.empty()) return in;
+
+		auto first_pos{ first_non_space(in) };
+		auto last_pos{ last_non_space(in) };
+
+		if (first_pos == in.end() || last_pos == in.rend()) return in;
+		
+		return std::string_view{ &*first_pos, static_cast<std::string_view::size_type>(std::distance(first_pos, last_pos.base())) };
+	}
+
+	auto parse_file_line(std::string_view line) -> std::pair<std::string_view, std::string_view> {
+		//Remove any line comments.
+		auto comment_start{ line.find('#') };
+		if (comment_start != std::string_view::npos) line.remove_suffix(line.size() - line.find('#'));
+
+		//Find the =
+		auto equals_pos{ std::find(line.begin(), line.end(), '=') };
+		if (equals_pos == line.end()) throw dp::ConfigReader::ConfigException("Line " + std::string{ line } + " is not empty, commented, and does not contain a =");
+
+
+
+		//I'll explain the cursed part of this
+		/*
+		*  In MSVC, string_view iterators aren't implemented as CharT* so in C++17 we need to do this silly dance of &*
+		*  We know at this point that the string contains at least one non-ws term (the =) so it's safe to do
+		*  The string_view iterator constructor is a C++20 feature so we need a call to std::distance and an ugly cast
+		*/
+		auto start{ first_non_space(line) };
+		//We have enough information to grab what we need now
+		std::string_view key_term{ &*start , static_cast<std::string_view::size_type>(std::distance(start, equals_pos)) };
+		++equals_pos;
+		std::string_view val_term{ &*equals_pos, static_cast<std::string_view::size_type>(std::distance(equals_pos,line.end())) };
+
+		return std::make_pair(trim(key_term), trim(val_term));
+	}
+
+	//We exploit the old case-insensitive char_traits trick for good comparison
+	template<typename T>
+	constexpr inline bool is_char_or_wchar = std::is_same_v<T, char> || std::is_same_v<T, wchar_t>;
+
+	template<typename T, std::enable_if_t<is_char_or_wchar<T>, bool> = true>
+	class ci_traits : public std::char_traits<T>{
+	
+		static T conv(T in) noexcept {
+			if constexpr (std::is_same_v<T, char>) {
+				return static_cast<char>(std::toupper(static_cast<unsigned char>(in)));
+			}
+			else {
+				return static_cast<wchar_t>(std::towupper(in));
+			}
+		}
+
+	public:
+
+		static bool eq(T lhs, T rhs) noexcept {
+			return conv(lhs) == conv(rhs);
+		}
+
+		static bool lt(T lhs, T rhs) noexcept {
+			return conv(lhs) < conv(rhs);
+		}
+
+		static int compare(const T* lhs, const T* rhs, std::size_t n){
+			while (n-- != 0)
+			{
+				if (conv(*lhs) < conv(*rhs))
+					return -1;
+				if (conv(*lhs) > conv(*rhs))
+					return 1;
+				++lhs;
+				++rhs;
+			}
+			return 0;		
+		}
+
+		static const T* find(const T* ptr, std::size_t n, T in_val) {
+			const auto val{ conv(in_val) };
+			while (n-- != 0) {
+				if (conv(*ptr) == val) return ptr;
+				++ptr;
+			}
+			return nullptr;
+		}	
+	};
+
+	using ci_view = std::basic_string_view<char, ci_traits<char>>;
+
+	template<typename dest_traits, typename CharT, typename src_traits>
+	std::basic_string_view<CharT, dest_traits> traits_cast(std::basic_string_view<CharT, src_traits> in) noexcept {
+		return { in.data(), in.size() };
+	}
+
+
+}
+
+
 
 namespace dp {
 
-	//ConfigReader::ConfigReader(std::string_view fileName, ConfigReader::flags inFlags) : m_flags{ inFlags } {
-	//	addFileToMap(fileName);
-	//}
 
-	ConfigReader::ConfigReader(std::filesystem::path file, ConfigReader::flags inFlags) : m_flags{ inFlags } {
-		addFileToMap(file.string());
-	}
+	void ConfigReader::addFile(std::filesystem::path in_file) {
+		if (!std::filesystem::exists(in_file)) throw ConfigException("Requested file does not exist");
 
-	void ConfigReader::close() {
-		m_values.clear();
-	}
+		std::ifstream file_input{ in_file };
+		std::string current_line{};
+		while (std::getline(file_input, current_line)) {			
+			auto first_char = first_non_space(current_line);
+			//Skip on empty lines and comment lines (start with #)
+			//Unfortunate cast needed for the comparison.
+			if (first_char == std::end(std::string_view{ current_line }) || *first_char == '#') continue;
 
-	//void ConfigReader::addFile(std::string_view fileName) {
-	//	addFileToMap(fileName);
-	//}
-
-	void ConfigReader::addFile(std::filesystem::path file) {
-		addFileToMap(file.string());
-	}
-
-	void ConfigReader::toLower(std::string& inString) {
-		std::transform(inString.begin(), inString.end(), inString.begin(), [](unsigned char c) -> char {return std::tolower(c); });
-	}
-
-
-	std::string ConfigReader::trim(const std::string& str, std::string_view whitespace) {
-		const auto strBegin = str.find_first_not_of(whitespace);
-		if (strBegin == std::string::npos) return "";
-
-		const auto strEnd = str.find_last_not_of(whitespace);
-		const auto strRange = strEnd - strBegin + 1;
-
-		return str.substr(strBegin, strRange);
-	}
-
-
-	void ConfigReader::addFileToMap(std::string_view fileName) {
-		std::ifstream file(std::string(fileName).c_str());
-		if (file.fail()) {
-			std::string errMsg{ "Error: File " };
-			errMsg += fileName;
-			errMsg += " could not be opened.";
-			throw ConfigReader::ConfigException(errMsg);
+			//Then insert a key-value pair separated by equals.
+			file_contents.insert(parse_file_line(current_line));
 		}
-		std::string currentLine;
-		while (std::getline(file, currentLine)) {
-			//Remove WS if needed.
-			if (m_flags & flags::removeAllWs) currentLine.erase(std::remove_if(currentLine.begin(), currentLine.end(), isspace), currentLine.end());
 
-			//Ignore commented or empty lines
-			if (currentLine[0] == '#' || currentLine.empty())continue;
-
-			if (m_flags & caseInsensitive)toLower(currentLine);
-
-			//Trim down comments at the end of lines
-			auto comment{ currentLine.find('#') };
-			if (comment != std::string::npos) {
-				currentLine = currentLine.substr(0, comment);
-			}
-
-			//Split the line using = as a delimiter.
-			auto splitPos{ currentLine.find('=') };
-			if (splitPos == std::string::npos) throw ConfigReader::ConfigException("Error: Non-empty, non-commented line in config file does not contain an =");
-
-			//Then use string_view to get two views of the line - one for each side of the delimiter
-			std::string lineBeforeEquals{ currentLine.substr(0,splitPos) };
-			std::string lineAfterEquals{ currentLine.substr(splitPos + 1, currentLine.length()) };
-			if (lineBeforeEquals.empty() || lineAfterEquals.empty()) throw ConfigReader::ConfigException("Error: Empty line before or after = in config file");
-
-			//If we're not otherwise keeping padding around the terms, we trim them down to just their text.
-			if (!(m_flags & keepAllPadding)) {
-				lineBeforeEquals = trim(lineBeforeEquals);
-				lineAfterEquals = trim(lineAfterEquals);
-			}
-
-			//And insert them
-			m_values.insert(std::make_pair(lineBeforeEquals, lineAfterEquals));
-		}
 	}
 
-	std::string_view ConfigReader::getValue(std::string_view key) {
-		return m_values.at(std::string{ key });
+	ConfigReader::ConfigReader(std::filesystem::path in_path) {
+		addFile(in_path);
 	}
 
+	auto  ConfigReader::GetValue(std::string_view key) const -> std::optional<std::string_view> {
 
+		auto val_pos = std::find_if(file_contents.begin(), file_contents.end(), [key = trim(key)](const map_type::value_type& in) {
+			auto ci_val = traits_cast<ci_traits<char>>(std::string_view(in.first));
+			return ci_val == traits_cast<ci_traits<char>>(key);
+		});
 
+		if (val_pos == file_contents.end()) return std::nullopt;
+		
+		return val_pos->second;
+	}
 
+	auto ConfigReader::clear() -> void {
+		file_contents.clear();
+	}
 
-
-
-	const char* ConfigReader::ConfigException::what() const noexcept {
-		return m_message.c_str();
+	auto ConfigReader::close() -> void {
+		clear();
 	}
 
 }
